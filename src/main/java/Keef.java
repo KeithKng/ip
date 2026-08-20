2d3093f628f9a119ed6db5b86fe2e3c152ee1e8d
KeithKng <keithkngjiaming@gmail.com>
2026-08-20 16:56:07 +0800

    Keef.java: Updated bot to store text and added list command

--- src/main/java/Keef.java
+++ src/main/java/Keef.java
@@ -1,32 +1,51 @@
 import java.util.Scanner;
 
 public class Keef {
-    public static void main(String[] args) {
+    private static final int MAX_TASKS = 100;
+    private static final String DIVIDER = "____________________________________________________________";
+
+   public static void main(String[] args) {
         String banner =
                 " _  __         __\n"
                 + "| |/ /___  ___ / _|\n"
                 + "| ' // _ \\/ _ \\ |_ \n"
                 + "| . \\  __/  __/  _|\n"
                 + "|_|\\_\\___|\\___|_|\n";
-        System.out.println("____________________________________________________________");
+        System.out.println(DIVIDER);
         System.out.println(banner);
         System.out.println("Hello! I'm Keef.");
         System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
+        System.out.println(DIVIDER);
 
         Scanner input = new Scanner(System.in);
+        String[] tasks = new String[MAX_TASKS];
+        int taskCount = 0;
         while (input.hasNextLine()) {
             String command = input.nextLine();
-            System.out.println("____________________________________________________________");
+            System.out.println(DIVIDER);
 
             if (command.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
-                System.out.println("____________________________________________________________");
+                System.out.println(DIVIDER);
                 break;
             }
 
-            System.out.println(command);
-            System.out.println("____________________________________________________________");
+            if (command.equals("list")) {
+                printTasks(tasks, taskCount);
+            } else if (taskCount < MAX_TASKS) {
+                tasks[taskCount] = command;
+                taskCount++;
+                System.out.println("added: " + command);
+            } else {
+                System.out.println("Sorry, the task list is full.");
+            }
+            System.out.println(DIVIDER);
+        }
+    }
+
+    private static void printTasks(String[] tasks, int taskCount) {
+        for (int i = 0; i < taskCount; i++) {
+            System.out.println((i + 1) + ". " + tasks[i]);
         }
     }
 }
