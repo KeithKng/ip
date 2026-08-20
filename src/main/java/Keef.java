4bc3b80db9f8d108a12e6ceef01c79af223113e4
KeithKng <keithkngjiaming@gmail.com>
2026-08-20 16:40:55 +0800

    Keef.java: Updated bot to echo commands

--- src/main/java/Keef.java
+++ src/main/java/Keef.java
@@ -1,12 +1,6 @@
-/**
- * Starts Keef, a chatbot that currently greets the user and exits.
- */
+import java.util.Scanner;
+
 public class Keef {
-    /**
-     * Prints Keef's initial greeting and farewell.
-     *
-     * @param args command-line arguments, which are not used
-     */
     public static void main(String[] args) {
         String banner =
                 " _  __         __\n"
@@ -19,7 +13,20 @@ public class Keef {
         System.out.println("Hello! I'm Keef.");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        Scanner input = new Scanner(System.in);
+        while (input.hasNextLine()) {
+            String command = input.nextLine();
+            System.out.println("____________________________________________________________");
+
+            if (command.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+
+            System.out.println(command);
+            System.out.println("____________________________________________________________");
+        }
     }
 }
